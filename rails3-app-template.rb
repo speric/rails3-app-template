f0e88dd275b3d5ab73edd53a48e796532e598f8f
speric <eric@prudentiadigital.com>
2012-11-13 10:36:43 -0500

    Updated README

--- rails3-app-template.rb
+++ rails3-app-template.rb
@@ -17,20 +17,18 @@ end
 puts "Installing gems..."
 run "bundle install"
 
-puts "Installing Bootstrap..."
-run "rails g bootstrap:install"
+
+puts "Creating README.markdown..."
+remove_file "README.rdoc"
+create_file "README.markdown"
 
 puts "Removing unnecessary default files..."
 remove_file "public/index.html"
 remove_file "app/assets/images/rails.png"
 
-puts "Appending .gitignore..."
-append_file '.gitignore' do <<-FILE
-'.DS_Store'
-'.rvmrc'
-'/config/database.yml'
-FILE
-end
+
+puts "Installing Bootstrap..."
+run "rails g bootstrap:install"
 
 puts "Creating database..."
 rake "db:create:all"
@@ -41,6 +39,14 @@ puts "Running migrations..."
 rake "db:migrate"
 rake "db:seed"
 rake "db:test:prepare"
+
+puts "Appending .gitignore..."
+append_file '.gitignore' do <<-FILE
+'.DS_Store'
+'.rvmrc'
+'/config/database.yml'
+FILE
+end
 
 puts "Initializing git and making first commit..."
 git :init
