e98d349b61188b8b12b298a464da3b5bb70044e9
speric <eric@prudentiadigital.com>
2012-11-13 10:30:33 -0500

    Output

--- rails3-app-template.rb
+++ rails3-app-template.rb
@@ -1,3 +1,6 @@
+puts "Modifying existing Rails app..."
+
+puts "Adding gems..."
 gem 'less-rails'
 gem 'twitter-bootstrap-rails'
 gem 'will_paginate', '~> 3.0'
@@ -7,29 +10,39 @@ group :assets do
 end
 
 gem_group :development do
-  gem "sextant", "~> 0.1.3"
-  gem "quiet_assets", "~> 1.0.1"
+  gem 'sextant', '~> 0.1.3'
+  gem 'quiet_assets', '~> 1.0.1'
 end
 
+puts "Installing gems..."
 run "bundle install"
+
+puts "Installing Bootstrap..."
 run "rails g bootstrap:install"
 
-# clean up default files
+puts "Removing unnecessary default files..."
 remove_file "public/index.html"
 remove_file "app/assets/images/rails.png"
 
-# exclude database configuration from source control
-append_file ".gitignore", "\n/config/database.yml"
+puts "Appending .gitignore..."
+append_file '.gitignore' do <<-FILE
+'.DS_Store'
+'.rvmrc'
+'/config/database.yml'
+FILE
+end
 
-# first time setup (cold boot)
+puts "Creating database..."
 rake "db:create:all"
+
+#authlogic stuff
+
+puts "Running migrations..."
 rake "db:migrate"
 rake "db:seed"
 rake "db:test:prepare"
 
-# intialize git and commit all the things
+puts "Initializing git and making first commit..."
 git :init
 git :add => "."
 git :commit => %Q{ -am 'Initial commit' }
-
-#authlogic stuff
